962cbb0c290e415362c958e3277ed5dae120c25c
JISU-YANG <jisu@ego.so>
2023-04-12 16:14:55 +0900

    [feat] 속도 개선을 위한 CDN 이미지 기능 추가
    - PathBuilder: jsdelivr의 CDN 링크로 변환
    - ImageDeliver: img 엘리먼트의 id 속성으로 CDN 링크를 src 속성으로 추가

---
+++ _includes/js/custom.js
@@ -0,0 +1,46 @@
+const PathResource = {
+    PROFILE: 'assets/',
+    PROJECT_REVIEW: 'docs/essay/project-review/resource/',
+
+}
+class PathBuilder{
+    CDN = {
+        _pathCDN: 'https://cdn.jsdelivr.net/gh/',
+        _pathPage: 'jisu-yang/jisu-yang.github.io/',
+        _directory: '',
+        _file: '',
+        category: function (category){
+            this._directory = category;
+            return this;
+        },
+        fileName: function (fileName){
+            this._file = fileName;
+            return this;
+        },
+        build: function () {
+            return this._pathCDN + this._pathPage + this._directory + this._file;
+        }
+    }
+}
+
+const ImageDeliver = {
+    _id: '',
+    _category: '',
+    _file: '',
+    id: function (id) {
+        this._id = id;
+        return this;
+    },
+    category: function (category) {
+        this._category = category;
+        return this;
+    },
+    file: function (file) {
+        this._file = file;
+        return this;
+    },
+    delivery: function (){
+        document.getElementById(this._id).setAttribute('src', new PathBuilder().CDN.category(this._category).fileName(this._file).build());
+        return this;
+    }
+}
